a2768f8eea979bffbd8ba9546a522d864d2b485b
jmengevik <jmengevik@158-37-230-21.hvl.no>
2019-11-11 16:22:05 +0100

    Ferdigstilt oppgave 5

--- src/no/hvl/dat100/jplab12/oppgave5/LesBlogg.java
+++ src/no/hvl/dat100/jplab12/oppgave5/LesBlogg.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
+import static java.lang.Integer.*;
 
 import no.hvl.dat100.jplab12.common.TODO;
 import no.hvl.dat100.jplab12.oppgave1.*;
@@ -23,8 +24,40 @@ public class LesBlogg {
 	private static String BILDE = "BILDE";
 
 	public static Blogg les(String filnavn) {
-
-		throw new UnsupportedOperationException(TODO.method());
-
+		
+		File fil = new File(MAPPE + filnavn);
+		Blogg samling;
+		
+		try {
+			Scanner leser = new Scanner(fil);
+			String linje = leser.nextLine();
+			int antallInnlegg = parseInt(linje);
+			samling = new Blogg(antallInnlegg);
+			for (int i = 0; i < antallInnlegg; i++) {
+				String type = leser.nextLine();
+				int id = parseInt(leser.nextLine());
+				String bruker = leser.nextLine();
+				String dato = leser.nextLine();
+				int likes = parseInt(leser.nextLine());
+				String tekst = leser.nextLine();
+				System.out.println(type  + " " + TEKST);
+				if (type.contentEquals(TEKST)) {
+					Tekst innlegg = new Tekst(id, bruker, dato, likes, tekst);
+					samling.leggTil(innlegg);
+					
+				}
+				if (type.contentEquals(BILDE)) {
+					String url = leser.nextLine();
+					Bilde innlegg = new Bilde(id, bruker, dato, likes, tekst, url);
+					samling.leggTil(innlegg);
+				}
+			}
+			leser.close();
+		}
+		catch (FileNotFoundException e) {
+			System.out.println("Filen som ble forsøkt lest, finnes ikke");
+			samling = new Blogg(0);
+		}
+		return samling;
 	}
 }
